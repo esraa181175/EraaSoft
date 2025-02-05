9f574c44ce1a90610210f7c02706f467c2b23f3b
esraa181175 <esraahassan146@gmail.com>
2025-02-05 22:38:37 +0200

    task 5-2

---
+++ Task 2-2-2025/main.js
@@ -0,0 +1,47 @@
+//-------ex 1 infinite loop---------------------------
+// let arr = [1, 2, 3, 4];
+// for (i = 1; i >= 0 ; i++) {
+//     console.log ( arr[i])
+// }
+
+
+
+// ------EX 2 returns the index of the string. --------
+//  let arr = [1, "ali", "ahmed", true, [4]]
+//  console.log(arr.indexOf("ali"));
+//  console.log(arr.indexOf("ahmed"));
+//  console.log(arr.indexOf(true))
+
+
+
+// --------EX 3  types of values ----------------------
+// let arr = [1, 2, 3, "ahmed", [15], true , null];
+// for (i = 0; i <= arr.length; i++) {
+//     console.log (typeof arr[i])
+// }
+
+
+
+// -------EX 4 Sum of all numbers ----------------------
+// let arr = [2, 3, 5,10,20,40,20];
+// let total = 0;
+// for (i = 0; i < arr.length; i++) {
+//     total += arr[i];
+// }
+// console.log("The Total is : " + total);
+
+
+
+// ---------EX 5 Total of all Odd Numbers----------------
+// let array = [1, 2, 3, 4, 5, 6, 7,1];
+// let total = 0;
+
+// for (i = 0; i < array.length; i += 1) {
+//     if (array[i] % 2 == 1) {
+//         total += array[i];
+//     } else {
+//         ("this is even number");
+//     }
+// }
+// console.log(total);
+
