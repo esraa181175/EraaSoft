26792fb6410f80029f43a24fbbdd5df83cf85867
esraa181175 <159013601+esraa181175@users.noreply.github.com>
2025-02-08 03:52:08 +0200

    Add files via upload

---
+++ Task 5-2-2025/main.js
@@ -0,0 +1,49 @@
+
+//__________Ex 1 area of Triangle___________________________________
+
+let base = prompt("Enter a base Value");
+let height = prompt("Enter a height value");
+
+let area = (base * height) / 2;
+console.log(area);
+
+
+//__________Ex 2 the Next Number from the Integer Passed______________
+
+// let number = 4;
+// number++;
+// // or
+// // number = number + 1;
+// console.log(number);
+
+
+//__________Ex 3 convert minutes to second____________________________
+
+// var minutes = prompt("Enter a minutes");
+// var minutes = minutes * 60;
+// console.log(minutes);
+
+//__________Ex 4 perimeter of Rectangle________________________________
+
+// let height = +prompt("Enter a height");
+// let width = +prompt("Enter a width");
+// let area = height + width;
+// let perimeter = (height + width) * 2;
+// console.log(area);
+// console.log(perimeter);
+
+//__________Ex 5 score Teams  __________________________________________
+
+
+// let First_Team = prompt(`Enter score first team : `) * 2;
+// let Second_Team = prompt(`Enter score Second team : `) * 3;
+// let Final_Result = First_Team + Second_Team;
+// console.log(Final_Result);
+
+//_______________________________________________________________________
+
+
+
+
+
+
